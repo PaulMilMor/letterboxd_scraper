6b7888b890abae63de6e722797e6b0525d3dc144
Paul Millanes <pauljmm99@gmail.com>
2022-11-16 22:50:18 -0700

    Get Name, Year and Rating from first page films

--- letterboxd_scraper/letterboxd_scraper/spiders/letterboxd.py
+++ letterboxd_scraper/letterboxd_scraper/spiders/letterboxd.py
@@ -2,10 +2,10 @@ import scrapy
 
 FILMS_OLDER = '//a[@class="next"]/@href'
 
-# FILM_URL = '//a[@class="frame"]/@href'
 FILM_URL = '//li[@class="poster-container"]/div/@data-target-link'
-FILM_TITLE = '//h1[contains(@class, "headline-1")]/text()'
-FILM_YEAR = '//a[contains(@href, "year/")]/text()'
+FILM_TITLE = '//section[@id="featured-film-header"]/h1/text()'
+FILM_YEAR = '//section[@id="featured-film-header"]/p/small/a/text()'
+# FILM_REVIEW = ''
 
 
 class FilmsSpider(scrapy.Spider):
@@ -45,18 +45,32 @@ class FilmsSpider(scrapy.Spider):
         # next_page_button = response.xpath(FILMS_OLDER).get()
 
         for link in films:
-            yield response.follow(link, callback=self.parse_film, cb_kwargs={'url': response.urljoin(link)})
+            rating = response.xpath(f'//li[div[contains(@data-target-link, "{link}")]]/p/span[1]/@class').get()
+            if rating != None:
+                rating = rating.split('-')
+                rating = int(rating[len(rating)-1])
+                if rating > 10:
+                    rating = 'None'
+                else:
+                    rating = rating / 2
+            else:
+                rating = 'None'
+
+
+            yield response.follow(link, callback=self.parse_film, cb_kwargs={'url': response.urljoin(link), 'rating': rating})
 
         # if next_page_button_link:
         #     yield response.follow(next_page_button, callback=self.parse_films, cb_kwargs={'films': films})
 
     def parse_film(self, response, **kwargs):
         link = kwargs['url']
-        title = response.xpath(FILM_TITLE)
-        year = response.xpath(FILM_YEAR)
+        rating = kwargs['rating']
+        title = response.xpath(FILM_TITLE).get()
+        year = response.xpath(FILM_YEAR).get()
 
         yield {
             'url': link,
             'title': title,
             'year': year,
+            'rating': rating
         }
