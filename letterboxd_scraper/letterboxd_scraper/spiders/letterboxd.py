5b3a20a8edc2a9fab8740da14084b22178d6e937
Paul Millanes <pauljmm99@gmail.com>
2022-11-17 23:51:32 -0700

    All Data from All Pages

--- letterboxd_scraper/letterboxd_scraper/spiders/letterboxd.py
+++ letterboxd_scraper/letterboxd_scraper/spiders/letterboxd.py
@@ -7,8 +7,14 @@ FILM_TITLE = '//section[@id="featured-film-header"]/h1/text()'
 FILM_YEAR = '//section[@id="featured-film-header"]/p/small/a/text()'
 FILM_COUNTRY = '//a[contains(@href, "/country/")]/text()'
 FILM_AVG_RATING = '//meta[@name="twitter:data2"]/@content'
-FILM_LANGUAGES = '//a[contains(@href, "language")]/text()'
+FILM_LANGUAGES = '//a[contains(@href, "/language/")]/text()'
 FILM_LENGTH = '//p[@class="text-link text-footer"]/text()'
+FILM_STUDIOS = '//a[contains(@href, "/studio/")]/text()'
+FILM_GENRES = '//a[contains(@href, "/genre/")]/text()'
+FILM_THEMES = '//a[contains(@href, "/theme/")]/text()'
+FILM_DIRECTOR = '//a[contains(@href, "/director/")]/text()'
+FILM_COMPOSER = '//a[contains(@href, "/composer/")]/text()'
+FILM_CAST = '//a[contains(@href, "/actor/")]/text()'
 
 class FilmsSpider(scrapy.Spider):
     name = 'films'
@@ -41,10 +47,8 @@ class FilmsSpider(scrapy.Spider):
 
     def parse(self, response):
 
-        # with open('resultado.html', 'w', encoding='utf-8') as f:
-        #     f.write(response.text)
         films = response.xpath(FILM_URL).getall()
-        # next_page_button = response.xpath(FILMS_OLDER).get()
+        next_page_button = response.xpath(FILMS_OLDER).get()
 
         for link in films:
             rating = response.xpath(f'//li[div[contains(@data-target-link, "{link}")]]/p/span[1]/@class').get()
@@ -61,19 +65,30 @@ class FilmsSpider(scrapy.Spider):
 
             yield response.follow(link, callback=self.parse_film, cb_kwargs={'url': response.urljoin(link), 'rating': rating})
 
-        # if next_page_button_link:
-        #     yield response.follow(next_page_button, callback=self.parse_films, cb_kwargs={'films': films})
+        if next_page_button:
+            yield response.follow(next_page_button, callback=self.parse)
+            
+
+    
+
 
     def parse_film(self, response, **kwargs):
-        link = kwargs['url']
-        rating = kwargs['rating']
+        if kwargs:
+            link = kwargs['url']
+            rating = kwargs['rating']
         title = response.xpath(FILM_TITLE).get()
         year = response.xpath(FILM_YEAR).get()
-        # country = response.xpath(FILM_COUNTRY).get()
-        average_rating =  response.xpath(FILM_AVG_RATING).get().split(' ')[0]
-        language = response.xpath(FILM_LANGUAGES).get()
         length = response.xpath(FILM_LENGTH).get()
         length = [int(s) for s in length.split() if s.isdigit()][0]
+        average_rating =  response.xpath(FILM_AVG_RATING).get().split(' ')[0]
+        country = response.xpath(FILM_COUNTRY).getall()
+        language = response.xpath(FILM_LANGUAGES).getall()
+        studios = response.xpath(FILM_STUDIOS).getall()
+        genres = response.xpath(FILM_GENRES).getall()
+        themes = response.xpath(FILM_THEMES).getall()
+        director = response.xpath(FILM_DIRECTOR).getall()
+        composer = response.xpath(FILM_COMPOSER).getall()
+        cast = response.xpath(FILM_CAST).getall()
 
         yield {
             'url': link,
@@ -81,7 +96,14 @@ class FilmsSpider(scrapy.Spider):
             'year': year,
             'length (mins)': length,
             'rating': rating,
-            # 'country': country,
             'average_rating': average_rating,
-            'language': language
+            'country': '/'.join(country),
+            'original_language': language[0],
+            'spoken_languages': '/'.join(set(language)),
+            'studio': '/'.join(studios),
+            'genres': '/'.join(genres),
+            'themes': '/'.join(themes),
+            'director': '/'.join([d for d in director if '\n' not in d and '\t' not in d ]),
+            'composer': '/'.join(composer),
+            'cast': '/'.join([c for c in cast if '\n' not in c and '\t' not in c])
         }
